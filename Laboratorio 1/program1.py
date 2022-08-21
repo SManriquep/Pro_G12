2e89d7c9bcdb558e5289fcd895154a759b16d83c
Gud2 <jmanriquep@unal.edu.co>
2022-08-21 18:44:23 -0500

    Se modifica el programa python para que reciba un caracter

--- Laboratorio 1/program1.py
+++ Laboratorio 1/program1.py
@@ -1,9 +1,8 @@
 # Editing the python code from the github web console
 
-bin_num = input ("Please write a binary number and then press enter:")
-dec_num = int(bin_num,2)
+
+char_ascii = input("Presione un caracter")
+dec_num = ord(char_ascii)
 hex_num = hex(dec_num)
-print ( bin_num , " in binary is equivalen to ", dec_num, " in decimal and to ", hex_num , "in hexadecimal" );
-
-
+print ( char_ascii , "\t", dec_num, "\t", hex_num , "\t", bin(dec_num) );
 
