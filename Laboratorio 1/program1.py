eda3796fbca795e56727f502425d79bc6f6c5947
SManriqueP <jmanriquep@unal.edu.co>
2022-08-21 18:07:01 -0500

    Ask for a number in the standard input.

--- Laboratorio 1/program1.py
+++ Laboratorio 1/program1.py
@@ -1,4 +1,4 @@
-bin_num = '11110101'
+bin_num = input ("Please write a binary number and then press enter:")
 dec_num = int(bin_num,2)
 hex_num = hex(dec_num)
 print ( bin_num , " in binary is equivalen to ", dec_num, " in decimal and to ", hex_num , "in hexadecimal" );
