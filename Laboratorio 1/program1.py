ccd5f7e6d19e2f2974ce715582627634ed7a30df
SManriqueP <jmanriquep@unal.edu.co>
2022-08-21 17:57:29 -0500

    Program to convert any binary number to hex

---
+++ Laboratorio 1/program1.py
@@ -0,0 +1,6 @@
+bin_num = '11110101'
+dec_num = int(bin_num,2)
+hex_num = hex(dec_num)
+print ( bin_num , " in binary is equivalen to ", dec_num, " in decimal and to ", hex_num , "in hexadecimal" );
+
+
